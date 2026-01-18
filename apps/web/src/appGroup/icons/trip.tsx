b50815c257288b24ff33944d700a5569b7e438d5
er.akash.srivastava <akash.srivastava1721+github@gmail.com>
2026-01-18 18:25:24 +0530

    add icons

---
+++ apps/web/src/appGroup/icons/trip.tsx
@@ -0,0 +1,7 @@
+export const TripIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M7 22v-1H5V6h4V2h6v4h4v15h-2v1h-2v-1H9v1H7Zm4-16h2V4h-2v2Zm1 6q1.325 0 2.588-.338T17 10.65V8H7v2.65q1.15.675 2.413 1.012T12 12Zm-1 3v-1.05q-1.05-.125-2.05-.375T7 12.9V19h10v-6.1q-.95.425-1.95.675T13 13.95V15h-2Zm1 0Zm0-3Zm0 .9Z" />
+        </svg>
+    )
+}
